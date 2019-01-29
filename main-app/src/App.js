b57c03a96d18bdc113b6d0ca07813399583fda90
Ryan <thesonicdoctor@gmail.com>
2019-01-29 16:01:59 -0500

    Added Dropdown
    With AutoComplete

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.scss';
 import MapboxComponent from './Mapbox';
+import HamburgerComponent from './LeftBurgerMenu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MapboxComponent/>
+        <HamburgerComponent pageWrapId={'page-wrap'} outerContainerId={'App'}/>
+        <main id='page-wrap'>
+          <MapboxComponent/>
+        </main>
       </div>
     );
   }
