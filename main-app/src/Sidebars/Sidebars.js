5eb430d6d3c2181a3ca12810dba2d2a07445c7bc
Ryan <thesonicdoctor@gmail.com>
2019-02-04 17:02:42 -0500

    Making Button
    to change state of sidebars

--- main-app/src/Sidebars/Sidebars.js
+++ main-app/src/Sidebars/Sidebars.js
@@ -10,39 +10,66 @@ const MenuItem = styled.h3`
   outline: none;
 `;
 
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #4ec3c7;
+  color: #4ec3c7;
+  padding: 0.25em 1em;
+`;
+
+const Container = styled.div`
+  text-align: left;
+`;
+
+const AutoSuggestContainer = styled.div`
+  height: 150px;
+`;
+
 class SidebarsComponent extends React.Component {
     constructor (props) {
       super(props)
       this.state = {
-        menuOpen: false
+        leftMenuOpen: false,
+        rightMenuOpen: false
       }
     }
   
     // This keeps your state in sync with the opening/closing of the menu
     // via the default means, e.g. clicking the X, pressing the ESC key etc.
     handleStateChange (state) {
-      this.setState({menuOpen: state.isOpen})  
+      this.setState({leftMenuOpen: state.isOpen})  
     }
     
     // This can be used to close the menu, e.g. when a user clicks a menu item
     closeMenu () {
-      this.setState({menuOpen: false})
+      this.setState({leftmenuOpen: false})
     }
   
     // This can be used to toggle the menu, e.g. when using a custom icon
     // Tip: You probably want to hide either/both default icons if using a custom icon
     // See https://github.com/negomi/react-burger-menu#custom-icons
     toggleMenu () {
-      this.setState({menuOpen: !this.state.menuOpen})
+      this.setState({leftMenuOpen: !this.state.leftMenuOpen})
     }
   
     render () {
         return (
           <div className='menu-sidebars'>
             <div className='menu-left'>
-              <Menu>
+              <Menu 
+              isOpen={this.state.leftMenuOpen}
+              onStateChange={(state) => this.handleStateChange(state)}
+              >
                 <MenuItem>Search</MenuItem>
-                <AutoSuggestComponent/>
+                <AutoSuggestContainer>
+                  <AutoSuggestComponent/>
+                </AutoSuggestContainer>
+                <Container>
+                  <Button href="" prefetch onClick={() => this.closeMenu()}>
+                    Submit
+                  </Button>
+                </Container>
               </Menu>
             </div>
             <div className='menu-right'>
