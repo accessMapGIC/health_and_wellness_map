d256b44a4181a445c9169ffe7c3c08a45791a166
Ryan <thesonicdoctor@gmail.com>
2019-02-07 14:55:27 -0500

    Added States to the Sidebars

--- main-app/src/Sidebars/Sidebars.js
+++ main-app/src/Sidebars/Sidebars.js
@@ -31,28 +31,40 @@ class SidebarsComponent extends React.Component {
       super(props)
       this.state = {
         leftMenuOpen: false,
-        rightMenuOpen: false
+        rightMenuOpen: false,
       }
     }
   
     // This keeps your state in sync with the opening/closing of the menu
     // via the default means, e.g. clicking the X, pressing the ESC key etc.
     handleStateChange (state) {
-      this.setState({leftMenuOpen: state.isOpen})  
+      this.setState({leftMenuOpen: state.leftMenuOpen})  
     }
     
-    // This can be used to close the menu, e.g. when a user clicks a menu item
-    closeMenu () {
-      this.setState({leftmenuOpen: false})
+    // This can be used to close the left menu, e.g. when a user clicks a menu item
+    closeMenuFactory(menu) { 
+      return () => {
+        if (menu === 0) {
+            this.setState({leftMenuOpen: false})
+        } else {
+            this.setState({rightMenuOpen: false})
+        }
+      }
     }
   
     // This can be used to toggle the menu, e.g. when using a custom icon
     // Tip: You probably want to hide either/both default icons if using a custom icon
     // See https://github.com/negomi/react-burger-menu#custom-icons
-    toggleMenu () {
-      this.setState({leftMenuOpen: !this.state.leftMenuOpen})
+    toggleMenu (menu) {
+      return () => {
+        if (menu === 0) {
+            this.setState({leftMenuOpen: !this.state.leftMenuOpen})
+        } else {
+            this.setState({rightMenuOpen: !this.state.rightMenuOpen})
+        }
+      }
     }
-  
+
     render () {
         return (
           <div className='menu-sidebars'>
@@ -66,7 +78,7 @@ class SidebarsComponent extends React.Component {
                   <AutoSuggestComponent/>
                 </AutoSuggestContainer>
                 <Container>
-                  <Button href="" prefetch onClick={() => this.closeMenu()}>
+                  <Button href="" prefetch onClick={this.closeMenuFactory(0)}>
                     Submit
                   </Button>
                 </Container>
@@ -85,21 +97,3 @@ class SidebarsComponent extends React.Component {
 
 
 export default SidebarsComponent;
-// export default props => {
-//   return (
-//       <div className='menu-sidebars'>
-//         <div className='menu-left'>
-//           <Menu>
-//             <MenuItem>Search</MenuItem>
-//             <AutoSuggestComponent/>
-//           </Menu>
-//         </div>
-//         <div className='menu-right'>
-//           <Menu right>
-//             <MenuItem>Search</MenuItem>
-//             <AutoSuggestComponent/>
-//           </Menu>
-//         </div>
-//       </div>
-//   );
-// }
